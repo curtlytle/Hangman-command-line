b17090c3a11de6b37971746a31cbfe6c5e8b5e93
Curtis Lytle <curtlytle@gmail.com>
2017-11-29 18:32:04 -0700

    Getting the command line prompt to work

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 // dependency for inquirer npm package
 var inquirer = require("inquirer");
+
+inquirer.prompt([
+    {
+        name: "letter",
+        message: "Guess a letter!"
+    }
+]).then(function (answer) {
+    console.log(answer);
+});
