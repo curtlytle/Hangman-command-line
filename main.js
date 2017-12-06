dd0fd8dd9a12628863498e8dd756839813187669
Curtis Lytle <curtlytle@gmail.com>
2017-12-05 19:12:30 -0700

    Getting end game working

--- main.js
+++ main.js
@@ -3,36 +3,49 @@ var inquirer = require("inquirer");
 var Word = require("./word");
 
 var gameWord = new Word("Baseball");
+var endGame = false;
 
 var chances = 3;
 var badGuesses = 0;
+var promptMsg = "Guess a letter!";
 
 function promptForGuess() {
     var letter = "";
     inquirer.prompt([
         {
             name: "letter",
-            message: "Guess a letter!"
+            message: promptMsg
         }
     ]).then(function (answer) {
         // console.log(answer);
         letter = answer.letter;
-        var goodGuess = gameWord.checkGuess(letter);
-        if (!goodGuess) {
-            console.log("Incorrect, " + (chances - badGuesses - 1) + " chances left.");
-            badGuesses++;
+        if (endGame) {
+            endGame = false;
+            if (letter.toUpperCase() === "Y") {
+                promptMsg = "Guess a letter!";
+                promptForGuess();
+            }
+        } else {
+            var goodGuess = gameWord.checkGuess(letter);
+            if (!goodGuess) {
+                console.log("Incorrect, " + (chances - badGuesses - 1) + " chances left.");
+                badGuesses++;
 
-            if (badGuesses >= chances) {
-                console.log("Game over, you lost.");
+                if (badGuesses >= chances) {
+                    console.log("Game over, you lost.");
+                    promptMsg = "Do you want to keep going? (Y or N)";
+                    endGame = true;
+                    promptForGuess();
+                } else {
+                    gameWord.displayWord();
+                    promptForGuess();
+                }
+            } else if (gameWord.isWordGuessed()) {
+                console.log("You WIN!  Word guessed was: " + gameWord.word);
             } else {
                 gameWord.displayWord();
                 promptForGuess();
             }
-        } else if (gameWord.isWordGuessed()) {
-            console.log("You WIN!  Word guessed was: " + gameWord.word);
-        } else {
-            gameWord.displayWord();
-            promptForGuess();
         }
     });
 }
